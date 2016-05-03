df3f510836a4d77a09ab14aa8bb9c702e5af4083
INOUE Takuya <inouetakuya5@gmail.com>
2016-05-03 23:28:55 +0900

    + componentDidMount: function () {

--- public/scripts/tutorial.js
+++ public/scripts/tutorial.js
@@ -51,11 +51,29 @@ var CommentForm = React.createClass({
 });
 
 var CommentBox = React.createClass({
+  getInitialState: function () {
+    return { data: [] };
+  },
+
+  componentDidMount: function () {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function (data) {
+        this.setState({ data: data });
+      }.bind(this),
+      error: function (xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  
   render: function () {
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList data={this.props.data} />
+        <CommentList data={this.state.data} />
         <CommentForm />
       </div>
     );
@@ -63,6 +81,6 @@ var CommentBox = React.createClass({
 });
 
 ReactDOM.render(
-  <CommentBox data={data} />,
+  <CommentBox url="api/comments" />,
   document.getElementById('content')
 );
