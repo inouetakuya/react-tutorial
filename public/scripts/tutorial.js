52787ddafe3950bcd5e7591e9311764fd67c7a93
INOUE Takuya <inouetakuya5@gmail.com>
2016-05-03 23:22:07 +0900

    + var commentNodes = this.props.data.map(function (comment) {

--- public/scripts/tutorial.js
+++ public/scripts/tutorial.js
@@ -1,3 +1,8 @@
+var data = [
+  { author: "Pete Hunt", text: "This is one comment" },
+  { author: "Jordan Walke", text: "This is *another* comment" }
+];
+
 var Comment = React.createClass({
   rawMarkup: function () {
     var rawMarkup = marked(this.props.children.toString(), { sanitize: true });
@@ -19,10 +24,17 @@ var Comment = React.createClass({
 
 var CommentList = React.createClass({
   render: function () {
+    var commentNodes = this.props.data.map(function (comment) {
+      return (
+        <Comment author={comment.author}>
+          {comment.text}
+        </Comment>
+      );
+    });
+
     return (
       <div className="commentList">
-        <Comment author="Pete Hunt">This is one comment</Comment>
-        <Comment author="Jorden Walke">This is *another* comment</Comment>
+        {commentNodes}
       </div>
     );
   }
@@ -43,7 +55,7 @@ var CommentBox = React.createClass({
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList />
+        <CommentList data={this.props.data} />
         <CommentForm />
       </div>
     );
@@ -51,6 +63,6 @@ var CommentBox = React.createClass({
 });
 
 ReactDOM.render(
-  <CommentBox />,
+  <CommentBox data={data} />,
   document.getElementById('content')
 );
